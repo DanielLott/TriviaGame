eeabd4b0b44dd3c1534c1c66b37173f03e14e684
Dan Lott <danieljlott@live.com>
2018-12-10 20:57:36 -0700

    checking mobile display #2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 var number = 100;
 $("button").click(function() {
-    $("button").hide();
-    $("br").hide();
-    $(".page-header").append("<hr></hr>");
-    $(".page-header").append("<h3 id='time-remaining'>Time remaining:<h3>");
-    $(".page-header").append("<h1 id='countdown-timer'>" + number + "<h1>")
+    // hiding button and extra line break
+    $("button, br").hide();
+    // adding line, text, countdown timer, text
+    $(".page-header").append("<hr></hr> <h3 id='time-remaining'>Time remaining:<h3> <h2 id='countdown-timer'>" + number + "<span id='really-plain'> seconds</span><h2>");
 
+ 
 });
